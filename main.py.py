7d3b1bee76fa9f0b39947da1ec621978533b81af
Manav0806 <145964558+Manav0806@users.noreply.github.com>
2024-01-23 17:55:35 +0530

    Delete main.py.py

--- main.py.py
+++
@@ -1,9 +0,0 @@
-import string as s
-import random as r
-splchar="!@#$%^&*()_"
-pswd = r.sample(s.ascii_letters+s.digits+splchar,8)
-pswd = "".join(pswd)
-# pswd = r.shuffle(pswd)
-print(pswd)
-print("\n")
-input("Press any key to exit.")
